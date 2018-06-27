4d1e8f1f3b4e2713c49b0307d0215db0c7d84e67
kevinkchiou <kevin.k.chiou@gmail.com>
2018-06-27 09:08:42 -0500

    kevinkchiou added flavor text to question

--- question.js
+++ question.js
@@ -1,7 +1,8 @@
-function Question(text, choices, answer) {
+function Question(text, choices, answer, flavor) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
+    this.flavor = flavor;
 }
 
 Question.prototype.isCorrectAnswer = function(choice) {
