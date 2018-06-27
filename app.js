0c03c560e5cc39f792c2602f7d40e48c79f75bb4
kevinkchiou <kevin.k.chiou@gmail.com>
2018-06-27 10:11:58 -0500

    kevinkchiou updated question format

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ function showScores() {
 
 // create questions
 var questions = [
-    new Question("Hepatitis C is a treatable, viral infection. What is the HCV incubation time?", ['1-2 days','1-2 weeks','2 weeks to 6 months','6 months to 1 year'],'2 weeks to 6 months'),
-	new Question("Approximately many people around the world are living with chronic HCV?",['700 thousand','7 million','70 million','700 million','7 billion'],'70 million'),
-    new Question("How many new HCV infections are there per year worldwide?",['800 thousand','2 million','20 million','40 million'],'2 million'),
-	new Question("Approximately what percentage of infected persons in the US are unaware of their infection?",['10%','25%','50%','75%','90%'],'50%'),
-    new Question("What are some of the disease complications related to HCV?",['Liver cancer','Liver cirrhosis / fibrosis','Liver failure','All of the above'],'All of the above')
-	new Question("Follow-up question; what is WHO's estimated number of deaths due to HCV complications per year worldwide?",['25 thousand','100 thousand','150 thousand','200 thousand','400 thousand'],'400 thousand')
+    new Question("Hepatitis C is a treatable, viral infection. What is the HCV incubation time?", ["1-2 days","1-2 weeks","2 weeks to 6 months","6 months to 1 year"],"2 weeks to 6 months","Did you know"),
+	new Question("Approximately many people around the world are living with chronic HCV?",["700 thousand","7 million","70 million","700 million","7 billion"],"70 million","Nearly 1 out of every 100 people suffer from chronic HCV infection"),
+    new Question("How many new HCV infections are there per year worldwide?",["800 thousand","2 million","20 million","40 million"],"2 million","Anywhere between 60%-85% of these go on to become chronic"),
+	new Question("Approximately what percentage of infected persons in the US are unaware of their infection?",["10%","25%","50%","75%","90%"],"50%","Although there aren't estimates for the rest of the world, the undiagnosed population is likely even higher within developing nations"),
+    new Question("What are some of the disease complications related to HCV?",["Liver cancer","Liver cirrhosis / fibrosis","Liver failure","All of the above"],"All of the above","The liver can regenerate even after removal of almost 90% of your liver. Chronic HCV infection causes this regenerative power to create more scar tissue and cell replication, inducing cirrhosis and cancerous liver cells")
+	new Question("Follow-up question; what is WHO's estimated number of deaths due to HCV complications per year worldwide?",["25 thousand","100 thousand","150 thousand","200 thousand","400 thousand"],"400 thousand","This is an incredible number of deaths that can be prevented with early treatment")
 ];
 
 // create quiz
